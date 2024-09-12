2a57285d66d8e2f8beaf92a68c3bfea9e3d090e4
alexmaragia <maragialex@gmail.com>
2024-09-12 23:10:20 +0300

    p3-aggregate-methods

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}
 
     def enroll(self, course):
         if isinstance(course, Course):
@@ -15,9 +17,25 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        return len(self._enrollments)
+
+    def set_grade(self, enrollment, grade):
+        if enrollment in self._enrollments:
+            self._grades[enrollment] = grade
+        else:
+            raise ValueError("Student is not enrolled in this course")
+
+    def aggregate_average_grade(self):
+        if not self._grades:
+            return 0
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        average_grade = total_grades / num_courses
+        return average_grade
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -30,6 +48,18 @@ class Course:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def get_students(self):
+        return [enrollment.student for enrollment in self._enrollments]
+
+    def average_course_grade(self):
+        total_grade = 0
+        num_students = 0
+        for enrollment in self._enrollments:
+            student_grade = enrollment.student._grades.get(enrollment)
+            if student_grade is not None:
+                total_grade += student_grade
+                num_students += 1
+        return total_grade / num_students if num_students > 0 else 0
 
 class Enrollment:
     all = []
@@ -45,3 +75,11 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
